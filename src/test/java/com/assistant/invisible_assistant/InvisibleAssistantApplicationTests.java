89300ec9ad355383ad48ab93d7cc90a300dbe4b1
EduAmerico <contato.eduamerico@gmail.com>
2025-04-08 14:04:31 -0300

    initial project setup with Spring Boot and dependencies

---
+++ src/test/java/com/assistant/invisible_assistant/InvisibleAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assistant.invisible_assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvisibleAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
